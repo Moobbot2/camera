506d7a69a0f176a4a2aeb08e3a68b723ed5c4a60
Ngô ĐứcTâm <133946215+Moobbot2@users.noreply.github.com>
2023-11-07 14:27:03 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,63 @@
+<!-- CSS -->
+<style>
+    #my_camera {
+        width: 320px;
+        height: 240px;
+        border: 1px solid black;
+    }
+</style>
+<center>
+    <!-- -->
+    <div id="my_camera"></div>
+    <!-- <input type=button value="Take Snapshot" onClick="take_snapshot()"> -->
+    <script>
+        // let i = 0;
+        // while (i < 1000) {
+        //     i++;
+        //     setTimeout(() => {
+        //         take_snapshot()
+        //     }, 1000);
+        // }
+    </script>
+    <div id="results"></div>
+
+    <!-- Script -->
+    <script type="text/javascript" src="webcam.min.js"></script>
+
+    <!-- Code to handle taking the snapshot and displaying it locally -->
+    <script language="JavaScript">
+
+        // Configure a few settings and attach camera
+        Webcam.set({
+            width: 640,
+            height: 480,
+            image_format: 'jpeg',
+            jpeg_quality: 90
+        });
+        Webcam.attach('#my_camera');
+
+        // preload shutter audio clip
+        var shutter = new Audio();
+        shutter.autoplay = true;
+        shutter.src = navigator.userAgent.match(/Firefox/) ? 'shutter.ogg' : 'shutter.mp3';
+
+        function take_snapshot() {
+            // play sound effect
+            shutter.play();
+
+            // take snapshot and get image data
+            Webcam.snap(function (data_uri) {
+
+                Webcam.upload(data_uri, 'saveimage.php', function (code, text, Name) {
+                    document.getElementById('results').innerHTML =
+                        '' +
+                        // display results in page
+                        //document.getElementById('results').innerHTML = 
+                        '<img src="' + data_uri + '"/>';
+                });
+
+
+            });
+        }
+
+    </script>
