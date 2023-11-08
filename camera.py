d93674370e98589b5cdb6996eac62b4a41be797b
Ngô ĐứcTâm <133946215+Moobbot2@users.noreply.github.com>
2023-11-08 13:27:00 +0700

    upload

--- camera.py
+++ camera.py
@@ -4,63 +4,48 @@ import cv2
 import numpy as np
 import base64
 import json
-import base64
-from PIL import Image
-from io import BytesIO
 
-async def handle_connection(websocket, path):
-    print('Handle Connection')
+async def on_message(websocket, path):
+    async for message in websocket:
+        data = json.loads(message)
+
+        if data["message"] == "sendMessage":
+            frame_data = data["data"]
+            await process_and_send_emotion(frame_data, websocket)
+
+async def process_and_send_emotion(frame_data, websocket):
+    if frame_data != 'data:,':
         
-    while True:
-        try:
-            print('Start connection')
-            message = await websocket.recv()
-            data = json.loads(message)
-            frame_data = data.get('data', '')  # Lấy dữ liệu hình ảnh từ 'data'
-            if frame_data != 'data:,':
-                print(frame_data)
-                # Decode the base64 data
-                image_data = base64.b64decode(frame_data)
+        frame_data = frame_data.split(',')[1]
+        # Decode the base64 frame data
+        decoded_data = base64.b64decode(frame_data)
+        # Convert frame data to a NumPy array
+        np_data = np.fromstring(decoded_data,np.uint8)
+        # Decode the image using OpenCV
+        image_data = cv2.imdecode(np_data,cv2.IMREAD_UNCHANGED)
 
-                # Create a BytesIO object to open the image with PIL
-                image_bytes = BytesIO(image_data)
+        # Encode the processed image back to base64
+        if not image_data.size:
+            print("Error: Invalid image data")
+        else:
+            # Continue with the image encoding
+            _, buffer = cv2.imencode('.jpg', image_data)
+            image_data_bytes = buffer.tobytes()
+            base64_data = base64.b64encode(image_data_bytes).decode('utf-8')
+            f64_decode = f'data:image/png;base64,{base64_data}'
+    else:
+        f64_decode = f'data:image/png;base64,'
 
-                # Open and display the image
-                image = Image.open(image_bytes)
-                image.show()
-                # Lỗi ở đây
-                # image_bytes = base64.b64decode(frame_data)
-                # image_data = cv2.imdecode(np.frombuffer(
-                #     image_bytes, np.uint8), cv2.IMREAD_COLOR)
-
-                # # Process the image if needed
-                # gray_frame = cv2.cvtColor(image_data, cv2.COLOR_BGR2GRAY)
-                # _, buffer = cv2.imencode('.jpg', gray_frame)
-                # image_data_bytes = buffer.tobytes()
-                # base64_data = base64.b64encode(image_data_bytes).decode('utf-8')
-            else:
-                base64_data = ''
-            # Convert the processed image to a base64-encoded data URI
-            f64_decode = base64_data
-            # f64_decode = f'data:image/jpeg;base64,{base64_data}'
-
-            # Send a response with the processed image data
-            json_data = {
-                'image': f64_decode,
-                'emotions': 'results'
-            }
-            json_string = json.dumps(json_data)
-            await websocket.send(json_string)
-        except Exception as e:
-            print(f"Error processing image: {str(e)}")
-            break
-
-global host_setup, port_setup
-host_setup = "192.168.88.30"
-port_setup = 4111
+    # Send the processed image back to the client
+    await websocket.send(json.dumps({
+        "message": "processedImage",
+        "image": f64_decode,
+    }))
 
 if __name__ == '__main__':
+    host_setup = "192.168.88.30"
+    port_setup = 4111
     print('check_is_socket is True')
-    start_server = websockets.serve(handle_connection, host_setup, port_setup)
+    start_server = websockets.serve(on_message, host_setup, port_setup)
     asyncio.get_event_loop().run_until_complete(start_server)
     asyncio.get_event_loop().run_forever()
