acf0a1620bb80711bef91e0e62797ff942364f1b
Ngô ĐứcTâm <133946215+Moobbot2@users.noreply.github.com>
2023-11-11 13:02:53 +0700

    test_frame

---
+++ get_frame_video.py
@@ -0,0 +1,31 @@
+import cv2
+import os
+
+def extract_frames(video_path, output_folder):
+    # Open the video file
+    cap = cv2.VideoCapture(video_path)
+
+    # Create the output folder if it doesn't exist
+    os.makedirs(output_folder, exist_ok=True)
+
+    # Get frames and save them to the output folder
+    frame_count = 0
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        frame_count += 1
+
+        # Save the frame as an image
+        frame_filename = os.path.join(output_folder, f"frame_{frame_count:04d}.png")
+        cv2.imwrite(frame_filename, frame)
+
+    # Release the video capture object
+    cap.release()
+
+if __name__ == "__main__":
+    video_path = "path/to/your/video.mp4"  # Change this to the path of your video file
+    output_folder = "path/to/output/folder"  # Change this to the desired output folder
+
+    extract_frames(video_path, output_folder)
